9e198c929e6f5812db84ced62c02c0db0a79a69b
Uwe Strahlendorf <ustrahlendorf@tecracer.de>
2021-12-29 18:52:46 +0100

    initial start with default template

---
+++ pokemon/App.vue
@@ -0,0 +1,11 @@
+<template>
+  App
+</template>
+
+<script>
+  export default {
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ pokemon/index.js
@@ -0,0 +1,8 @@
+// import * as Vue from 'vue/dist/vue.esm-bundler.js'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+
+const app = createApp(App)
+
+app.mount('#app')
